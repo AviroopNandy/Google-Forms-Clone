104c3979d298a88ffb6cd67eee498ec4ba5cc7fc
mounvip1525 <mounvip1525@gmail.com>
2021-02-23 23:13:52 +0530

    top bar

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -6,6 +6,10 @@ import ListAltOutlinedIcon from '@material-ui/icons/ListAlt';
 import DeleteOutlinedIcon from '@material-ui/icons/DeleteOutlined';
 import HomeOutlinedIcon from '@material-ui/icons/HomeOutlined';
 import PermContactCalendarOutlinedIcon from '@material-ui/icons/PermContactCalendarOutlined';
+import AddOutlinedIcon from '@material-ui/icons/AddOutlined';
+import PermIdentityOutlinedIcon from '@material-ui/icons/PermIdentityOutlined';
+import PermMediaOutlinedIcon from '@material-ui/icons/PermMediaOutlined';
+import SearchOutlinedIcon from '@material-ui/icons/SearchOutlined';
 export default class Home extends Component {
     render() {
         return (
@@ -16,12 +20,36 @@ export default class Home extends Component {
                        <ul>
                            <li><HomeOutlinedIcon className={styles.sideIcons}/><h4>Home</h4></li>
                            <li><ListAltOutlinedIcon className={styles.sideIcons}/><h4>Forms</h4></li>
+                           <li><PermMediaOutlinedIcon className={styles.sideIcons}/><h4>Folders</h4></li>
                            <li><DeleteOutlinedIcon className={styles.sideIcons}/><h4>Bin</h4></li>
                            <li><PermContactCalendarOutlinedIcon className={styles.sideIcons}/><h4>Contact</h4></li>
                        </ul>
                    </div>
-                   <div className={styles.formsContainer}>
-                       <hi>lorem</hi>
+                   <div className={styles.mainContainer}>
+                        <div className={styles.top}>
+                            <div className={styles.search}>
+                                <input placeholder="Search"></input>
+                                <SearchOutlinedIcon />
+                            </div>
+                            <div className={styles.topLeft}>
+                                <div className={styles.profile} title="Create form"><AddOutlinedIcon /></div>
+                                <div className={styles.profile} title="Your Profile"><PermIdentityOutlinedIcon /></div>
+                            </div>
+                        </div>
+                        <div className={styles.carouselContainer}>
+                            <div>hello</div>
+                            <div>hello</div>
+                            <div>hello</div>
+                        </div>
+                        All Forms
+                        <div className={styles.formsContainer}>
+                            <div>Form1</div>
+                            <div>Form1</div>
+                            <div>Form1</div>
+                            <div>Form1</div>
+                            <div>Form1</div>
+                            <div>Form1</div>
+                        </div>
                    </div>
                 </div>
             </React.Fragment>
