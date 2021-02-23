bcd9b2b2c02502c07a572c7f95fea7598dae4ade
mounvip1525 <mounvip1525@gmail.com>
2021-02-23 15:47:08 +0530

    setup files and structure

---
+++ src/containers/Home/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import styles from './Home.module.css';
+
+
+export default class Home extends Component {
+    render() {
+        return (
+            <h1>Hi from home</h1>
+        )
+    }
+}
+
