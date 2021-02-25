59649827fc4499dc4c24d66bff8e00659a95cdf6
mounvip1525 <mounvip1525@gmail.com>
2021-02-25 22:57:32 +0530

    contact page

--- src/containers/Contact-Us/Contact-us.js
+++ src/containers/Contact-Us/Contact-us.js
@@ -1,10 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import styles from './Contact-us.module.css';
 
 export default class Contact extends Component {
     render() {
         return (
             <React.Fragment>
-                
+                <div className={styles.wrapper}>
+                <div className={styles.container}>
+                    <div className={styles.left}>
+                        <div>
+                            <h2>Contact Us</h2>
+                            <p>We are always happy to help you and we will try our best to get back to you within a day. Feedback appreciated!</p>
+                        </div>
+                    </div>
+                    <div className={styles.right}>
+                        bruhh
+                    </div>
+                </div>
+                </div>
             </React.Fragment>
         )
     }
